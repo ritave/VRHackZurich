630ec44ee77377d1e44db0a8590767e69123ee38
Olaf Tomalka <olaf.tomalka@gmail.com>
2015-10-03 11:27:29 +0200

    Polishing, starting to work on hands

---
+++ VRUnity/Assets/Scenes/Main/Scripts/MyPlayerController.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(PositionProvider))]
+public class MyPlayerController : MonoBehaviour
+{
+	private static readonly Vector3 FOWARD_VEC = new Vector3(0, 0, 1);
+	private static readonly Vector3 BACKWARDS_VEC = new Vector3(0, 0, -1);
+	private static readonly Vector3 LEFT_VEC = new Vector3(-1, 0, 0);
+	private static readonly Vector3 RIGHT_VEC = new Vector3(1, 0, 0);
+
+	public GameObject m_player;
+	public GameObject m_leftHand;
+	public GameObject m_rightHand;
+
+	private OVRCameraRig m_cameraRig;
+	private PositionProvider m_positionProvider;
+
+	private Vector3 m_realLifeLHandPos;
+	private Vector3 m_realLifeRHandPos;
+	private Vector3 m_realLifeBodyPos;
+	
+	private void Awake()
+	{
+		m_cameraRig = GetComponentInChildren<OVRCameraRig>();
+		m_positionProvider = GetComponent<PositionProvider>();
+	}
+
+	private void FixedUpdate()
+	{
+		GetData();
+		UpdateBody();
+		UpdateHands();
+	}
+
+	private void GetData()
+	{
+		m_positionProvider.GetData(
+			ref m_realLifeLHandPos,
+			ref m_realLifeRHandPos,
+			ref m_realLifeBodyPos
+		);
+	}
+
+	private void UpdateBody()
+	{
+		m_player.transform.position = new Vector3(
+			m_realLifeBodyPos.x,
+			m_player.transform.position.y,
+			m_realLifeBodyPos.z	
+		);
+	}
+
+	private void UpdateHands()
+	{
+
+	}
+}
---
+++ VRUnity/Assets/Scenes/Main/Scripts/PositionProvider.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Threading;
+
+public class PositionProvider : MonoBehaviour
+{
+	private Thread m_listenThread;
+	private Vector3 m_body;
+	private Vector3 m_lHand;
+	private Vector3 m_rHand;
+	private Object m_threadLock = new Object();
+	private volatile bool m_shouldRun;
+
+	public void GetData(ref Vector3 leftHand, ref Vector3 rightHand, ref Vector3 body)
+	{
+		lock (m_threadLock)
+		{
+			leftHand = m_lHand;
+			rightHand = m_rHand;
+			body = m_body;
+		}
+	}
+
+	private void Start()
+	{
+		StartThread();
+	}
+
+	private void StartThread()
+	{
+		m_shouldRun = true;
+		m_listenThread = new Thread(this.Listen);
+		m_listenThread.Start();
+	}
+
+	private void StopThread()
+	{
+		m_shouldRun = false;
+		m_listenThread.Interrupt();
+		m_listenThread.Join();
+	}
+
+
+	private void Listen()
+	{
+		System.Random myRandom = new System.Random();
+		while (m_shouldRun)
+		{
+			try
+			{
+				lock (m_threadLock)
+				{
+					m_body = new Vector3(
+						m_body.x + (float)myRandom.Next(11) / 100.0f - 0.05f,
+						0,
+						m_body.z + (float)myRandom.Next(11) / 100.0f - 0.05f
+					);
+				}
+				Thread.Sleep(50);
+			} catch (ThreadInterruptedException)
+			{
+				// Empty on purpose
+			}
+		}
+	}
+}
